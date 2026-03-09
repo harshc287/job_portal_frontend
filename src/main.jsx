9459522b43c25c0c1aaf551859746860243e4316
harshc287 <chouhanharsh332@gmail.com>
2026-03-09 23:14:07 +0530

    bug solved in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot, ReactDOM  } from 'react-dom/client'
+import   ReactDOM   from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "./styles/global.css"
